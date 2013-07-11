46d29495e23491ccca3d54c88fd0626c4a8c8ad3
jhalak <jhalak1983@yahoo.com>
2013-07-11 15:59:36 +0600

    Initin commit

---
+++ assets/www/js/channel-info.js
@@ -0,0 +1,55 @@
+function initDatabase() {
+	db = openDatabase("Database", "1.0", "CHANNELS", 200000);
+	db.transaction(pupulateTables, errorCB, successCB);
+}
+
+function pupulateTables(tx) {
+	//tx.executeSql('DROP TABLE IF EXISTS INFO');
+	tx.executeSql("CREATE TABLE IF NOT EXISTS INFO (id unique, name, number)");
+}
+
+function errorCB() {
+	console.log("Error pupulating database");
+}
+
+function successCB() {
+	console.log("Done pupulating database");
+}
+
+function getDatabase() {
+	db = openDatabase("Database", "1.0", "CHANNELS", 200000);
+	return db;
+}
+
+function ChannelListController($scope) {
+	var db = getDatabase();
+	var id, rows;
+	db.transaction(function(tx){
+		tx.executeSql("SELECT * FROM INFO", [], function(tx, results){
+			id = parseInt(results.rows.length + 1);
+		});
+	});
+	
+	$scope.channels = [
+	    {name: "BTV", number: 1},
+	    {name: "ETV", number: 2}
+	];
+	
+	$scope.addChannel = function(){
+		var cName = $scope.FormChannelName;
+		var cNumber = $scope.FormChannelNumber;
+		
+		$scope.channels.push({name:cName, number:cNumber});
+		
+		db.transaction(function(tx) {
+			tx.executeSql("INSERT INTO INFO (id, name, number) VALUES ("+ id +", '" + cName +  "', '" + cNumber + "')");
+	      }, function(err) {
+	      		alert("Error processing SQL for inserting channel: "+ err.code);
+	      }, function() {
+	      		alert('Saved entry to database' );
+	    });
+		
+		$scope.FormChannelName = '';
+		$scope.FormChannelNumber = '';
+	}
+}
